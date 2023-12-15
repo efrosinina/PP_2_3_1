e14d787f574e0862463290cb63f044909198832e
efrosinina <e.efrosininaa@gmail.com>
2023-12-15 13:28:45 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,56 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String printAll(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String addNewUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new";
+    }
+
+    @PostMapping()
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/edit")
+    public String edit(@RequestParam Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import web.model.User;
+import java.util.List;
+
+public interface UserService {
+
+    void saveUser(User user);
+
+    User updateUser(User user);
+
+    void deleteUser(Long id);
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+}
